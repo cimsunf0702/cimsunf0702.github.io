048aadabd1b053bd9df462b80f58cfbb80a98b2a
Tai Nguyen <cimsunf@gmail.com>
2021-08-06 17:14:23 +0700

    test upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,15 +4,15 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "a938a00f79af685306b0de1ff30c2354",
-"index.html": "1ff986ca7c94ad3185dba39275ae3c38",
-"/": "1ff986ca7c94ad3185dba39275ae3c38",
-"main.dart.js": "581ca48b73cd1dec495056027e9eb799",
+"index.html": "8030a8a6af87393056d70814ecd59a9d",
+"/": "8030a8a6af87393056d70814ecd59a9d",
+"main.dart.js": "0a532e97eb137974eaa88467ce9ac042",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "a47d7b2d50d93c942ee502950e6999f4",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/NOTICES": "a434fb9b5e03a268d9bc7aa707ebb721",
+"assets/NOTICES": "d753f26517a030ecbec05218f372670e",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1"
